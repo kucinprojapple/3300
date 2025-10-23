72ea3f5dbf7674a822a9fb22c6c548ebf875f966
vitalii kharlan <vitalii.kharlan@itrident.agency>
2025-10-23 15:44:19 +0300

    third

--- lib/view/loading_screen.dart
+++ lib/view/loading_screen.dart
@@ -15,7 +15,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.green,
-        title: Center(child: Text('Green Gym Club new')),
+        title: Center(child: Text('Green Gym Club new new')),
       ),
       body: Container(color: Colors.green),
     );
