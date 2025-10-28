abc314e06ce04ed2c75635b7851b45aa6f122ca3
vitalii kharlan <vitalii.kharlan@itrident.agency>
2025-10-28 12:19:47 +0200

    add webp images instead of png images

--- lib/app_core_design/assets.dart
+++ lib/app_core_design/assets.dart
@@ -2,70 +2,123 @@ sealed class AppAssets {
   // png images
   static const _basePath = 'assets/images/';
 
-  /// images for Background
-  static const backgroundMain = '${_basePath}background_main.png';
-  static const shirt = '${_basePath}shirt.png';
+  // /// images for Background
+  // static const backgroundMain = '${_basePath}background_main.png';
+  // static const shirt = '${_basePath}shirt.png';
+  //
+  // /// images for Button
+  // static const buttonMain = '${_basePath}button_main.png';
+  //
+  // /// images for Grid
+  // static const slotContainer = '${_basePath}slot_container.png';
+  // static const glowLine = '${_basePath}glow_line.png';
+  //
+  // /// images for Avatar
+  // static const avatarClearPlace = '${_basePath}avatar_clear_place.png';
+  // static const avatarMan = '${_basePath}avatar_man.png';
+  // static const avatarWoman = '${_basePath}avatar_woman.png';
+  //
+  // /// images for Photo
+  // static const backgroundMakeAPhoto = '${_basePath}background_make_a_photo.png';
+  // static const backgroundChoosePhoto =
+  //     '${_basePath}background_choose_photo.png';
+  // static const backgroundCancel = '${_basePath}background_cancel.png';
+  //
+  // /// images for Exercises
+  // static const exerciseBicycleCrunches =
+  //     '${_basePath}exercise_bicycle_crunches.png';
+  // static const exerciseBurpees = '${_basePath}exercise_burpees.png';
+  // static const exerciseCrunches = '${_basePath}exercise_crunches.png';
+  // static const exerciseJumpingJacks = '${_basePath}exercise_jumping_jacks.png';
+  // static const exerciseLegRaises = '${_basePath}exercise_leg_raises.png';
+  // static const exerciseLunges = '${_basePath}exercise_lunges.png';
+  // static const exercisePushUps = '${_basePath}exercise_push_ups.png';
+  // static const exerciseSquats = '${_basePath}exercise_squats.png';
+  // static const exerciseTricepDips = '${_basePath}exercise_tricep_dips.png';
+  //
+  // /// images for Game
+  // static const wow = '${_basePath}wow.png';
+  // static const timer_1 = '${_basePath}timer_1.png';
+  // static const timer_2 = '${_basePath}timer_2.png';
+  // static const timer_3 = '${_basePath}timer_3.png';
+  // static const timerGo = '${_basePath}timer_go.png';
+  // static const timerGoodJob = '${_basePath}timer_good_job.png';
+  // static const timerExcellent = '${_basePath}timer_excellent.png';
+  // static const timerAmazing = '${_basePath}timer_amazing.png';
+  // static const timerWellDone = '${_basePath}timer_well_done.png';
+  //
+  // /// images for icons
+  // static const iconAdd = '${_basePath}icon_add.png';
+  // static const iconBack = '${_basePath}icon_back.png';
+  // static const iconEdit = '${_basePath}icon_edit.png';
+  // static const iconInfo = '${_basePath}icon_info.png';
+  // static const iconPause = '${_basePath}icon_pause.png';
+  // static const iconPlay = '${_basePath}icon_play.png';
+  // static const iconProfile = '${_basePath}icon_profile.png';
+  // static const iconQuestion = '${_basePath}icon_question.png';
+  // static const iconSettings = '${_basePath}icon_settings.png';
+  // static const iconTimerPlay = '${_basePath}icon_timer_play.png';
+  // static const iconTimerStop = '${_basePath}icon_timer_stop.png';
 
-  // static const backgroundShop = '${_basePath}background_shop.png';
+  // // webp images
+  // static const _basePath = 'assets/images/';
+  //
+  /// images for Background
+  static const backgroundMain = '${_basePath}background_main.webp';
+  static const shirt = '${_basePath}shirt.webp';
 
   /// images for Button
-  static const buttonMain = '${_basePath}button_main.png';
-
-  // static const menuButton = '${_basePath}menu_button.png';
-  // static const leaderboardButton = '${_basePath}leaderboard_button.png';
-  // static const changeLevelActiveButton =
-  //     '${_basePath}change_level_active_button.png';
-  // static const changeLevelInactiveButton =
-  //     '${_basePath}change_level_inactive_button.png';
+  static const buttonMain = '${_basePath}button_main.webp';
 
   /// images for Grid
-  static const slotContainer = '${_basePath}slot_container.png';
-  static const glowLine = '${_basePath}glow_line.png';
+  static const slotContainer = '${_basePath}slot_container.webp';
+  static const glowLine = '${_basePath}glow_line.webp';
 
   /// images for Avatar
-  static const avatarClearPlace = '${_basePath}avatar_clear_place.png';
-  static const avatarMan = '${_basePath}avatar_man.png';
-  static const avatarWoman = '${_basePath}avatar_woman.png';
+  static const avatarClearPlace = '${_basePath}avatar_clear_place.webp';
+  static const avatarMan = '${_basePath}avatar_man.webp';
+  static const avatarWoman = '${_basePath}avatar_woman.webp';
 
   /// images for Photo
-  static const backgroundMakeAPhoto = '${_basePath}background_make_a_photo.png';
+  static const backgroundMakeAPhoto =
+      '${_basePath}background_make_a_photo.webp';
   static const backgroundChoosePhoto =
-      '${_basePath}background_choose_photo.png';
-  static const backgroundCancel = '${_basePath}background_cancel.png';
+      '${_basePath}background_choose_photo.webp';
+  static const backgroundCancel = '${_basePath}background_cancel.webp';
 
   /// images for Exercises
   static const exerciseBicycleCrunches =
-      '${_basePath}exercise_bicycle_crunches.png';
-  static const exerciseBurpees = '${_basePath}exercise_burpees.png';
-  static const exerciseCrunches = '${_basePath}exercise_crunches.png';
-  static const exerciseJumpingJacks = '${_basePath}exercise_jumping_jacks.png';
-  static const exerciseLegRaises = '${_basePath}exercise_leg_raises.png';
-  static const exerciseLunges = '${_basePath}exercise_lunges.png';
-  static const exercisePushUps = '${_basePath}exercise_push_ups.png';
-  static const exerciseSquats = '${_basePath}exercise_squats.png';
-  static const exerciseTricepDips = '${_basePath}exercise_tricep_dips.png';
+      '${_basePath}exercise_bicycle_crunches.webp';
+  static const exerciseBurpees = '${_basePath}exercise_burpees.webp';
+  static const exerciseCrunches = '${_basePath}exercise_crunches.webp';
+  static const exerciseJumpingJacks = '${_basePath}exercise_jumping_jacks.webp';
+  static const exerciseLegRaises = '${_basePath}exercise_leg_raises.webp';
+  static const exerciseLunges = '${_basePath}exercise_lunges.webp';
+  static const exercisePushUps = '${_basePath}exercise_push_ups.webp';
+  static const exerciseSquats = '${_basePath}exercise_squats.webp';
+  static const exerciseTricepDips = '${_basePath}exercise_tricep_dips.webp';
 
   /// images for Game
-  static const wow = '${_basePath}wow.png';
-  static const timer_1 = '${_basePath}timer_1.png';
-  static const timer_2 = '${_basePath}timer_2.png';
-  static const timer_3 = '${_basePath}timer_3.png';
-  static const timerGo = '${_basePath}timer_go.png';
-  static const timerGoodJob = '${_basePath}timer_good_job.png';
-  static const timerExcellent = '${_basePath}timer_excellent.png';
-  static const timerAmazing = '${_basePath}timer_amazing.png';
-  static const timerWellDone = '${_basePath}timer_well_done.png';
+  static const wow = '${_basePath}wow.webp';
+  static const timer_1 = '${_basePath}timer_1.webp';
+  static const timer_2 = '${_basePath}timer_2.webp';
+  static const timer_3 = '${_basePath}timer_3.webp';
+  static const timerGo = '${_basePath}timer_go.webp';
+  static const timerGoodJob = '${_basePath}timer_good_job.webp';
+  static const timerExcellent = '${_basePath}timer_excellent.webp';
+  static const timerAmazing = '${_basePath}timer_amazing.webp';
+  static const timerWellDone = '${_basePath}timer_well_done.webp';
 
   /// images for icons
-  static const iconAdd = '${_basePath}icon_add.png';
-  static const iconBack = '${_basePath}icon_back.png';
-  static const iconEdit = '${_basePath}icon_edit.png';
-  static const iconInfo = '${_basePath}icon_info.png';
-  static const iconPause = '${_basePath}icon_pause.png';
-  static const iconPlay = '${_basePath}icon_play.png';
-  static const iconProfile = '${_basePath}icon_profile.png';
-  static const iconQuestion = '${_basePath}icon_question.png';
-  static const iconSettings = '${_basePath}icon_settings.png';
-  static const iconTimerPlay = '${_basePath}icon_timer_play.png';
-  static const iconTimerStop = '${_basePath}icon_timer_stop.png';
+  static const iconAdd = '${_basePath}icon_add.webp';
+  static const iconBack = '${_basePath}icon_back.webp';
+  static const iconEdit = '${_basePath}icon_edit.webp';
+  static const iconInfo = '${_basePath}icon_info.webp';
+  static const iconPause = '${_basePath}icon_pause.webp';
+  static const iconPlay = '${_basePath}icon_play.webp';
+  static const iconProfile = '${_basePath}icon_profile.webp';
+  static const iconQuestion = '${_basePath}icon_question.webp';
+  static const iconSettings = '${_basePath}icon_settings.webp';
+  static const iconTimerPlay = '${_basePath}icon_timer_play.webp';
+  static const iconTimerStop = '${_basePath}icon_timer_stop.webp';
 }
